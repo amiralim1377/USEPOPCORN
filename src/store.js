576df08c1197ac101128fabd0997d6dc034dd293
amirali <alimoradi7718@gmail.com>
2024-10-31 23:00:56 +0330

    store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import watchlistReducer from "./Reducer/watchlistReducer";
 
+const persistConfing = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  watchlist: watchlistReducer,
+});
+
+const persistedReducer = persistReducer(persistConfing, reducer);
+
 const store = configureStore({
-  reducer: {
-    watchlist: watchlistReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
