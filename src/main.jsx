eee34dba69eeb07fac241c38318992bab9ef1813
amirali <alimoradi7718@gmail.com>
2024-10-31 22:57:14 +0330

    main change

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store.js";
-
+import { PersistGate } from "redux-persist/es/integration/react";
+import { persistStore } from "redux-persist";
+const persistor = persistStore(store);
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
