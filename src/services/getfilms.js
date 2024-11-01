d84a349019ae9119d1122688ec78a20db27f254e
amirali <alimoradi7718@gmail.com>
2024-11-01 12:58:14 +0330

    change url

--- src/services/getfilms.js
+++ src/services/getfilms.js
@@ -3,7 +3,7 @@ const KEY = 55232223;
 export async function getfilms(film) {
   // console.log(film);
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&S=${film}`);
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&S=${film}`);
   if (!res.ok) {
     throw new Error("Network response was not ok");
   }
--- src/services/getfilmsbyid.js
+++ src/services/getfilmsbyid.js
@@ -3,7 +3,7 @@ const KEY = 55232223;
 export async function getfilmsbyid(imdbID) {
   // console.log(film);
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${imdbID}`);
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${imdbID}`);
   if (!res.ok) {
     throw new Error("Network response was not ok");
   }
